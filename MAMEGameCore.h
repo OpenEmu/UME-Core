e3562864ce38a88caec2e0f6a63dd7e1a06cec41
PGGB <dnagel@outlook.com>
2013-07-30 21:35:06 +0200

    Changed some build stuff

--- MAMEGameCore.h
+++ MAMEGameCore.h
@@ -34,11 +34,8 @@ OE_EXPORTED_CLASS
 @interface MAMEGameCore : OEGameCore
 
 - (void)osd_init:(running_machine *)machine;
-
 - (void)osd_exit:(running_machine *)machine;
-
 - (void)osd_update:(bool)skip_redraw;
-
 - (void)osd_update_audio_stream:(const INT16 *)buffer samples:(int)samples_this_frame;
 
 @end
